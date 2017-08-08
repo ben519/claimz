04fd315db0cb52c949176901eb07c7fefb498c02
Ben Gorman <bgorman519@gmail.com>
2017-08-08 15:28:46 -0500

    changed check_claims() to is_valid_claims() and improved docs

--- R/check_claims.R
+++ R/check_claims.R
@@ -1,10 +1,27 @@
 #' @title
-#' Check Claims
+#' Is Valid claims
 #'
 #' @description
 #' Validate a claims dataset
+#'
+#' @details
+#' Checks claims dataset for valid columns, values, and other properties.  Returns TRUE if valid, FALSE otherwise.
+#'
+#' @param claims data.table object with columns {"ClaimID", "PolicyID", "DOL"}
+#'
+#' @import data.table
+#'
+#' @examples
+#' library(data.table)
+#'
+#' # Sample claims
+#' claimz
+#'
+#' # Validate
+#' check_claims(claimz)
+#' check_claims(claimz[, c("PolicyID", "EffectiveDate")])
 
-check_claims <- function(claims){
+is_valid_claims <- function(claims){
   # Check the claims dataset for expected properties
 
   #--------------------------------------------------
@@ -12,13 +29,22 @@ check_claims <- function(claims){
 
   fields.claims <- c("ClaimID", "PolicyID", "DOL")
   missingfields.claims <- setdiff(fields.claims, colnames(claims))
-  if(length(missingfields.claims > 0))
+  if(length(missingfields.claims > 0)){
     warning(paste0("These fields missing from claims:{", paste(missingfields.claims, collapse=", "), "}"))
+    return(FALSE)
+  }
 
   #--------------------------------------------------
   # Check ID for uniqueness
 
   dupes.ClaimID <- sum(duplicated(claims$ClaimID))
-  if(dupes.ClaimID > 0)
+  if(dupes.ClaimID > 0){
     warning(paste("claims contains", dupes.ClaimID, "duplicate ClaimIDs"))
+    return(FALSE)
+  }
+
+  #--------------------------------------------------
+  # Return TRUE
+
+  return(TRUE)
 }
